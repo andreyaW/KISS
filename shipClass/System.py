eee28e254c6b0708cbb11111fc67b93c2c093baa
Andreya <adware@umich.edu>
2025-04-08 14:35:36 -0400

    updating system structure function

--- shipClass/System.py
+++ shipClass/System.py
@@ -10,7 +10,7 @@ class System():
         self.components = comps
         self.parallels = parallels
         self.history = []
-        self.state = SolveStructureFunction()
+        self.state = self.SolveStructureFunction()
         
     def simulate(self, number_of_steps: int) -> None:
         ''' update the state of the system '''
@@ -22,26 +22,47 @@ class System():
                 self.history.append(self.state)
 
 
+    def get_key_by_value(my_dict, value):
+        """
+        Returns the key associated with the given value in the dictionary.
+        If the value is not found, it returns None. If multiple keys have the same value,
+        it returns the first key found.
+        """
+        for key, val in my_dict.items():
+            if val == value:
+                return key
+        return None
+
+
+
+    def getStates(self):
+        """gets the states of the systems components as number values instead of strings"""
+        
+        comp_states = np.zeros(len(self.components))
+        for comp in self.components:
+            comp_states[i] = get_key_by_value(comp.states, comp.sensedState)
+
+        return comp_states
+
+
+
     def SolveStructureFunction(self):
 
         ''' calculate the structure function of the system '''
         
-        Xi = np.zeros(len(self.components))
-        for i,comp in enumerate(self.components):
-            
-            if comp.sensedState == 0 or comp.sensedState == 1:
-                Xi[i] = 1
-                
-            if comp.sensedState >= 2:
-                Xi[i] = 0  
-
-        # for series comps 
-        phi = min(Xi)
-
-        # # for parallel comps
-        # phi = max(Xi)
+        Xi = self.getStates()
+        phi = min(Xi)       # for series comps
+        # phi = max(Xi)      # for parallel comps
 
         return phi
+
+
+
+        # for i,state in enumerate(comp_states):
+        #     if state == 0 or comp.sensedState == 1:
+        #         Xi[i] = 1
+        #     if comp.sensedState >= 2:
+        #         Xi[i] = 0  
         
         
         
